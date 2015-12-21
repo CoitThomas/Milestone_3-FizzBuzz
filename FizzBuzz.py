32416a7838901a170a02c53addad737a44a11974
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2015-12-21 12:42:48 -0600

    Clean up code using Pylint

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,26 +1,27 @@
-from types import *
+from types import ListType, IntType
 
 def create_list(size):
-	# takes one positive int 'size' and returns a list of all integers 0 to 'size'  
-	assert type(size) is IntType, "The 'size' of the list needs to be an integer."
-	list = []
-	for i in range(0,size+1):
-		list.append(i)
-	return list
-	
-def insert_something(someList,someThing,divisor):
-	# takes a list 'someList', a desired variable to be inserted into the list 'someThing', and an int 'divisor'
-	# 'someThing' gets inserted into 'someList' at every element where the element's index is divisible by 'divisor'
-	assert type(someList) is ListType, "A list was not given."
-	assert type(divisor) is IntType, "The positions of insertion need to be an integer."
-	for n,i in enumerate(someList):
-		if n%divisor == 0 and n!=0:
-			someList[n]=someThing
-	return someList
-				
+    #takes one positive int 'size' and returns a list of all integers 0 to 'size'
+    assert isinstance(size, IntType)
+    some_list = []
+    for i in range(0, size+1):
+        some_list.append(i)
+    return some_list
+
+def insert_something(some_list, some_thing, divisor):
+    #takes a list 'someList', a desired variable to be inserted into the list 'someThing', and an
+    #int 'divisor'. 'someThing' gets inserted into 'someList' at every element where the element's
+    #index is divisible by 'divisor'
+    assert isinstance(some_list, ListType)
+    assert isinstance(divisor, IntType)
+    for index, content in enumerate(some_list):
+        if index%divisor == 0 and index != 0:
+            some_list[index] = some_thing
+    return some_list
+
 new_list = create_list(100)
 insert_something(new_list, 'fizz', 3)
 insert_something(new_list, 'buzz', 5)
 insert_something(new_list, 'fizzbuzz', 15)
 
-print new_list			
+print new_list
