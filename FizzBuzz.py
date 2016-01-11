e60ece7c2255df80cb07fe7cb839efc4c3354ed6
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-01-11 14:25:52 -0600

    Add module and function docstrings. Capitlize inserted strings.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,17 +1,24 @@
+"""This module creates a list of 100 enumerated integers and inserts
+'Fizz' into the list at every point divisible by 3, 'Buzz' into the
+list at every point divisible by 5, and 'FizzBuzz' at every point
+divisible by both 3 and 5. The list is then printed out.
+"""
 from types import ListType, IntType
 
 def create_list(size):
-    #takes one positive int 'size' and returns a list of all integers 0 to 'size'
+    """Create an enumerated integer list of length, 'size'."""
     assert isinstance(size, IntType)
     some_list = []
     for i in range(0, size+1):
         some_list.append(i)
     return some_list
 
-def insert_something(some_list, some_thing, divisor):
-    #takes a list 'someList', a desired variable to be inserted into the list 'someThing', and an
-    #int 'divisor'. 'someThing' gets inserted into 'someList' at every element where the element's
-    #index is divisible by 'divisor'
+def insert_something(some_list, anything, divisor):
+    """Insert an unknown variable into a list.
+
+    Insert a variable at every point in a list where the index of the
+    variable is divisible by an int variable, 'divisor'.
+    """
     assert isinstance(some_list, ListType)
     assert isinstance(divisor, IntType)
     for index, content in enumerate(some_list):
@@ -20,8 +27,8 @@ def insert_something(some_list, some_thing, divisor):
     return some_list
 
 new_list = create_list(100)
-insert_something(new_list, 'fizz', 3)
-insert_something(new_list, 'buzz', 5)
-insert_something(new_list, 'fizzbuzz', 15)
+insert_something(new_list, 'Fizz', 3)
+insert_something(new_list, 'Buzz', 5)
+insert_something(new_list, 'FizzBuzz', 15)
 
 print new_list
