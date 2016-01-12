e7895174a5a32f47e07431bb402dfb4d06c26b69
Coit Scharringhausen <coit.scharringhausen@gmail.com>
2016-01-12 00:00:53 -0600

    Refactor is_appropriate(number) into a pure predicate. Elaborate on the test module for is_appropriate to include False assertions.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,38 +4,34 @@ print 'Fizz' instead of the number, for multiples of five, print
 print 'FizzBuzz'.
 """
 def is_appropriate(number):
-    """Assert whether or not a given number is an integer between 0 and
-    101. Is so, return the boolean True.
+    """Return True if the given number is an integer between 0 and
+    101. Otherwise, return False.
     """
-    assert isinstance(number, int)
-    assert 0 < number <= 100
-    return True
+    if isinstance(number, int) and 0 < number <= 100:
+        return True
+    return False
 
 def determine_element(element):
     """Determine whether the element should be named, 'Fizz', 'Buzz',
     'FizzBuzz', or itself, make the change to the element(if
     applicable), and return it.
     """
-    if is_appropriate(element):
-        if element % 15 == 0:
-            return 'FizzBuzz'
-        elif element % 3 == 0:
-            return 'Fizz'
-        elif element % 5 == 0:
-            return 'Buzz'
-        return element
+    assert is_appropriate(element) is True
+    if element % 15 == 0:
+        return 'FizzBuzz'
+    elif element % 3 == 0:
+        return 'Fizz'
+    elif element % 5 == 0:
+        return 'Buzz'
+    return element
 
 def format_element(number, element):
-    """Structure the number according to the amount of digits it
-    contains and print it out with its corresponding element.
+    """Arrange the given number and element to fit the following
+    format: {ddd  :  element} where the number 'ddd' is given a
+    right-side alignment.
     """
-    if is_appropriate(number):
-        if number < 10:
-            return "  " + str(number) + "  :  "+str(element)
-        if 10 <= number < 100:
-            return " " + str(number) + "  :  "+str(element)
-        if number == 100:
-            return str(number) + "  :  "+str(element)
+    assert is_appropriate(number) is True
+    return "{0:3d}  :  ".format(number) + str(element)
 
 for integer in range(1, 101):
     ELEMENT = determine_element(integer)
--- test_is_appropriate.py
+++ test_is_appropriate.py
@@ -1,9 +1,17 @@
 """Verify if the function is_appropriate(number) returns True when an
-appropriate number is given."""
+appropriate number is given and False when it is not."""
 from FizzBuzz import is_appropriate
 
 def test_is_appropriate():
-    """Assert that integers between 0 and 101 return True."""
+    """Assert that integers between 0 and 101 return True and that all
+    other inputs return False.
+    """
     assert is_appropriate(1) is True
     assert is_appropriate(100) is True
     assert is_appropriate(53) is True
+    assert is_appropriate(0) is False
+    assert is_appropriate(101) is False
+    assert is_appropriate(50.5) is False
+    assert is_appropriate('a') is False
+    assert is_appropriate('hello') is False
+    assert is_appropriate(['hello', 'world']) is False
